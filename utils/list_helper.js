2400032b2b612ed0a4e2f862e1f19325f04479b4
Viktor Svider <viktor1svider@gmail.com>
2023-10-04 00:01:58 +0300

    add 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (likes, blog) => {
+        return blog.likes + likes
+    }
+    const likes = blogs.reduce(reducer, 0)
+    return likes
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
